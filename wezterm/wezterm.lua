9d14e9f9d3cdf6a377957ef6202cd93aa816d8ab
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2025-05-14 23:30:43 +0530

    add transparency toggle to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local Config = require('config')
 
 return Config:init()
   :append(require('config.appearance'))
+  :append(require('config.transparency'))
   :append(require('config.font'))
   :append(require('config.launch'))
   :append(require('config.bindings')).options
