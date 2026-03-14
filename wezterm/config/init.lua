9373bca4724ae51cf4d8e7b86fc377e39bf083a3
prnsh <58560660+pranshuj73@users.noreply.github.com>
2026-03-14 17:42:09 +0530

    disable wezterm gui startup resize

--- wezterm/config/init.lua
+++ wezterm/config/init.lua
@@ -1,16 +1,16 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-wezterm.on("gui-startup", function(cmd)
-  local screen = wezterm.gui.screens().main
-  local ratio = 0.6
-  local width, height = screen.width * ratio, screen.height * ratio
-  local tab, pane, window = wezterm.mux.spawn_window(cmd or {
-    position = { x = (screen.width - width) / 2, y = (screen.height - height) / 2 },
-  })
-  -- window:gui_window():maximize()
-  window:gui_window():set_inner_size(width, height)
-end)
+-- wezterm.on("gui-startup", function(cmd)
+--   local screen = wezterm.gui.screens().main
+--   local ratio = 0.6
+--   local width, height = screen.width * ratio, screen.height * ratio
+--   local tab, pane, window = wezterm.mux.spawn_window(cmd or {
+--     position = { x = (screen.width - width) / 2, y = (screen.height - height) / 2 },
+--   })
+--   -- window:gui_window():maximize()
+--   window:gui_window():set_inner_size(width, height)
+-- end)
 
 ---@class Config
 ---@field options table
