910c0dc1439c1f8d17262375cf1952743f3f7455
prnsh <58560660+pranshuj73@users.noreply.github.com>
2026-04-11 12:48:39 +0530

    fix bspwm + polybar launch & reload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 DIR="$HOME/.config/polybar"
+
 # Terminate already running bar instances
-killall -q polybar
+pkill -9 polybar 2>/dev/null || true
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 polybar -q -r main -c "$DIR"/config.ini &
-
